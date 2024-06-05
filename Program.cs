3bf5d19ad6333f1b1edd500c4ecf087758383d30
UmarADev <umargithub03@gmail.com>
2024-06-05 23:46:44 +0500

    Fibonacci finish

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+int fibonacci1 = 0;
+int fibonacci2 = 1;
+
+Console.Write("son kiriting: ");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+
+while(fibonacci1 <= userNumber)
+{
+    int nextFibonacci = fibonacci1 + fibonacci2;
+    fibonacci1 = fibonacci2;
+    fibonacci2 = nextFibonacci;
+    if (fibonacci1 == userNumber)
+    {
+        Console.WriteLine("Bu fibonacci son");
+        break;
+    }
+}
+
+if (fibonacci1 != userNumber)
+{
+    Console.WriteLine("Bu fibonacci son emas");
+}
